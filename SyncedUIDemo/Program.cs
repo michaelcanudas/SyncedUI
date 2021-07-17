b1b459c508a41f874bec7276756ac629692a3240
PatAPizza56 <63616729+PatAPizza56@users.noreply.github.com>
2021-07-16 23:12:30 -0400

    Initial commit
    Initial commit

---
+++ SyncedUIDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using SyncedUI.Window;
+using System.Drawing;
+using static SyncedUI.Synced;
+
+namespace SyncedUIDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var window = new MainWindow();
+
+            using var graphics = window.CreateGraphics();
+
+            var brush = new SolidBrush(Color.Red);
+
+            window.Layout();
+
+            while (window.IsOpen())
+            {
+                window.HandleEvents();
+
+                graphics.Clear(Color.Black);
+
+                window.Render(graphics);
+            }
+        }
+
+        class MainWindow : SyncedWindow
+        {
+            public override void Layout()
+            {
+                TextSection()
+                    .Children(Text("hey!").Color(Color.White));
+            }
+        }
+    }
+}
